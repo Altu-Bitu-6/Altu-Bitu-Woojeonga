b2b09490d8f4ec09618449e961109af6c81f5fa9
ktde24 <kjjgbjbe24@gmail.com>
2024-02-22 09:28:06 +0900

    [정렬, 맵, 셋] 2월22일

--- 01_정렬_맵_셋/19636.cpp
+++ 01_정렬_맵_셋/19636.cpp
@@ -1,42 +1,64 @@
 #include <iostream>
+#include <tuple>
+#include <cmath> // 절대값, 소숫점 처리를 위한 헤더 파일
 using namespace std;
 
+// 다이어트 결과 계산 함수
+tuple<int, int, int> diet(int w0, int i0, int i, int a, int d, int t) {
+    // w: 몸무게, m: 기초대사량
+    int w1 = w0, m1 = i0; // 기초대사량 변화 고려하지 않는 경우의 체중, 기초대사량
+    int w2 = w0, m2 = i0; // 기초대사량 변화 고려한 경우의 체중, 기초대사량
+
+    while (d--) {
+        // 체중 변화 계산: 일일 에너지 섭취량(i) - (기초대사량 + 일일 활동 대사량(a)) 만큼 변화
+        // 일일 에너지 소비량 = 일일 기초 대사량 + 일일 활동 대사량
+        w1 += i - (m1 + a); // 기초대사량 변화 고려X
+        w2 += i - (m2 + a); // 기초대사량 변화 고려O
+
+        // 기초대사량 변화 계산 (m2만 변화)
+        if (abs(i - (m2 + a)) > t) {
+            // (일일 에너지 섭취량 - 일일 에너지 소비량)의 절대값이 기초 대사량 변화 역치(t)를 초과하는 경우
+            // 음수의 나눗셈에 주의 => float끼리의 나눗셈으로 수행하고 소숫점 버림
+                                       
+            m2 += float(i - (m2 + a)) / 2.0;
+        }
+    }
+    // 계산된 체중, 기초대사량 반환
+    return {w1, w2, m2};
+}
 int main() {
-    int W0, I0, T, D, I, A;
-    cin >> W0 >> I0 >> T >> D >> I >> A;
+    int w0, i0, i, a;
+    int d, t;
+    int final_weight, final_meta;
 
-    // 일일 기초 대사량 변화를 고려하지 않은 경우
-    int noChangeWeight = W0 + (I - (I0 + A)) * D; // 예상 체중
-    if (noChangeWeight <= 0) { // 예상 체중이 0 이하라면
+    // 입력 받기
+    // w0: 다이어트 전 체중, i0: 다이어트 전 일일 에너지 섭취량 및 기초대사량, t: 기초 대사량 변화 역치
+    // d: 다이어트 기간, i: 다이어트 기간 일일 에너지 섭취량, a: 다이어트 기간 일일 활동 대사량
+    cin >> w0 >> i0 >> t;
+    cin >> d >> i >> a;
+
+    // 다이어트 결과 계산
+    tuple<int, int, int> tmp = diet(w0, i0, i, a, d, t);
+
+    int w1 = get<0>(tmp), m1 = i0;          // 기초대사량 변화 고려 X, tmp 튜플의 첫 번째 요소를 추출
+    int w2 = get<1>(tmp), m2 = get<2>(tmp); // 기초대사량 변화 고려 O 
+
+    if (w1 <= 0) {
+        // m1은 i0에서 변화하지 않고, i0는 0보다 크므로 기초대사량이 0kcal 이하가 되지 않음
         cout << "Danger Diet\n";
     } else {
-        cout << noChangeWeight << " " << I0 << endl; // 예상 체중 및 일일 기초 대사량 출력
+        cout << w1 << " " << m1 << "\n";
     }
 
-    // 일일 기초 대사량 변화를 고려한 경우
-    int changeWeight = W0, changeI = I0;
-    bool isDead = false, yoyo = false;
-    for (int day = 0; day < D; ++day) {
-        int energyDiff = I - (changeI + A);
-        changeWeight += energyDiff; // 체중 변화
-        if (abs(energyDiff) > T) { // 기초 대사량 변화 역치 초과하면,
-            changeI += energyDiff / 2; // 일일 기초 대사량 변화
-        }
-        if (changeWeight <= 0 || changeI <= 0) { // 사망 조건
-            isDead = true;
-            break;
-        }
-    }
-
-    if (isDead) {
-        cout << "Danger Diet\n";
+    if (w2 <= 0 || m2 <= 0) {
+        cout << "Danger Diet";
     } else {
-        // 요요 현상 판단
-        if (changeI + A < I0) {
-            yoyo = true;
+        cout << w2 << " " << m2 << " ";
+        if (m2 < i0) {
+            cout << "YOYO";
+        } else {
+            cout << "NO";
         }
-        cout << changeWeight << " " << changeI << " " << (yoyo ? "YOYO" : "NO") << endl;
     }
-
     return 0;
 }
